7f02b13519c7a16437baa7cf93f071998535994f
geekymoose <constantin.masson@umontreal.ca>
2018-05-13 08:58:46 -0400

    Add compiler flags in CMake for debug

--- include/engine/GameObject.h
+++ include/engine/GameObject.h
@@ -18,6 +18,8 @@ class GameObject {
 
     protected:
         glm::vec3 position;
+        glm::vec3 scale;
+        glm::vec3 rotation;
 
 
     // -------------------------------------------------------------------------
